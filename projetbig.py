cfd42313ae26117928d93174988a88d05b117d5a
Moustapha Toure <tourealmamy1202@gmail.com>
2020-12-11 18:19:13 +0100

    first commit

---
+++ projetbig.py
@@ -0,0 +1,22 @@
+__auteurs__ = "TOURE Almamy Moustapha, OUX Philippe"
+# initialisation
+
+
+from pyspark import SparkContext
+sc = SparkContext(master="local", appName="bigdata")
+
+# lecture du RDD
+bd = sc.textFile("sample.txt")
+print(bd.collect())
+
+# la phase de comptage
+mot = bd.flatMap(lambda line: line.split())\
+    .map(lambda word: (word, 1))
+print(mot.collect())
+
+# affichage cle valeur
+motcount= mot.reduceByKey(lambda a, b: a + b)
+print(motcount.collect())
+
+# exportation du fichier
+motcount.coalesce(1).saveAsTextFile("big_projet.txt")
